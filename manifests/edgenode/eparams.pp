e322965aa76bc41154a19c89c0b26705b0433b07
root <root@puppet.example.com>
2017-07-26 22:46:59 +0000

    Fixing the pull issues with the script

--- manifests/edgenode/eparams.pp
+++ manifests/edgenode/eparams.pp
@@ -3,8 +3,8 @@ class glic::edgenode::eparams  {
  ## Copy the configs to and from S3
  $cfgscript 		= "edgenodecfgs.sh"
  $cfgscriptpath		= "/tmp/glic"
- $cronuser 		= "ubuntu"
- $s3bucketdev		= "glic-test-bucket"
+ $cronuser 		= "ec2-user"
+ $s3bucketdev		= "glic-test-bucket/configs"
  $s3bucketprod		= ""
  $s3backupprefix	= "cfgbackups"
 
